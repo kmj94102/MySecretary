6787ed42a2f1fe9aac59e17aad9659ef152b1448
minjae <kmj94102@gmail.com>
2021-10-12 16:35:37 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mysecretary/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mysecretary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+// 날씨 Lottie 배경 컬러 #86B9DE
+// 전철 Lottie 배경 컬러 #589675
